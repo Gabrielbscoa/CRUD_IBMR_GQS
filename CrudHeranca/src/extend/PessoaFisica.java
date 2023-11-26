53e013f0f4e950a0fb33682427b5f2ec88f06492
Gabrielbscoa <107504001+Gabrielbscoa@users.noreply.github.com>
2023-11-25 22:18:31 -0300

    Update PessoaFisica.java

--- CrudHeranca/src/extend/PessoaFisica.java
+++ CrudHeranca/src/extend/PessoaFisica.java
@@ -4,28 +4,24 @@ public class PessoaFisica extends Pessoa {
     
     private String cpf;
 
-    
-    public PessoaFisica(String nome, int idade, String cpf) {
+     public PessoaFisica(String nome, int idade, String cpf) {
         super(nome, idade);
         this.cpf = cpf;
     }
 
-   
     public String getCpf() {
         return cpf;
     }
 
-   
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-   
     @Override
     public String toString() {
         return "PessoaFisica{" +
-                "id=" + getId() +
                 ", nome='" + getNome() + '\'' +
+                ", idade=" + getIdade() +
                 ", cpf='" + cpf + '\'' +
                 '}';
     }
