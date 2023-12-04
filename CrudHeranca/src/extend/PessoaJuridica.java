aec73074932af5018c56d8422a6e94a67dd1a0ef
Gabrielbscoa <107504001+Gabrielbscoa@users.noreply.github.com>
2023-12-04 14:42:54 -0300

    Update PessoaJuridica.java

--- CrudHeranca/src/extend/PessoaJuridica.java
+++ CrudHeranca/src/extend/PessoaJuridica.java
@@ -1,29 +1,50 @@
-package extend;
+public class PessoaJuridica extends Pessoa implements PessoaPessoa {
+    private long cnpj;
 
-public class PessoaJuridica extends Pessoa {
-    
-    private String cnpj;
-
-    public PessoaJuridica(String nome, int idade, String cnpj) {
-        super(nome, idade);
-        this.cnpj = cnpj;
-       
+    public PessoaJuridica() {
     }
 
-   public String getCnpj() {
+    public PessoaJuridica(String nome, long cnpj) {
+        super(nome);
+        this.cnpj = cnpj;
+    }
+
+    public long getCnpj() {
         return cnpj;
     }
 
-   public void setCnpj(String cnpj) {
+    public void setCnpj(long cnpj) {
         this.cnpj = cnpj;
     }
 
     @Override
-    public String toString() {
-        return "PessoaJuridica{" +
-                ", nome='" + getNome() + '\'' +
-                ", idade=" + getIdade() +
-                ", cnpj='" + cnpj + '\'' +
-                '}';
+    public void criar() {
+        
+        System.out.println("Criando Pessoa Jurídica: " + getNome());
+    }
+
+    @Override
+    public void ler() {
+        
+        System.out.println("Lendo Pessoa Jurídica: " + getNome());
+    }
+
+    @Override
+    public void atualizar() {
+        
+        System.out.println("Atualizando Pessoa Jurídica: " + getNome());
+    }
+
+    @Override
+    public void deletar() {
+        
+        System.out.println("Deletando Pessoa Jurídica: " + getNome());
+    }
+
+
+    @Override
+    public String getNome() {
+        
+        return "Pessoa Jurídica: " + super.getNome();
     }
 }
