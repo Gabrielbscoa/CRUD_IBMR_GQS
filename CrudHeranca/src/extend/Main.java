9960c114d76b6bab824caea5c07cee1ef187979d
Gabrielbscoa <107504001+Gabrielbscoa@users.noreply.github.com>
2023-12-04 14:45:49 -0300

    Update Main.java

--- CrudHeranca/src/extend/Main.java
+++ CrudHeranca/src/extend/Main.java
@@ -1,25 +1,12 @@
-package extend;
-
 public class Main {
     public static void main(String[] args) {
         
-        PessoaFisica pessoaFisica = new PessoaFisica("Maria", 25, "123.456.789-00");
-        System.out.println("Pessoa Física:");
-        System.out.println(pessoaFisica.toString());
+        PessoaPessoa operacoesPessoaFisica = new PessoaFisica("João", 123456789);
+        operacoesPessoaFisica.criar();
+        operacoesPessoaFisica.ler();
 
-       
-        PessoaJuridica pessoaJuridica = new PessoaJuridica("Empresa XYZ", 10, "12345/0001-67", "XYZ Ltda");
-        System.out.println("\nPessoa Jurídica:");
-        System.out.println(pessoaJuridica.toString());
-
-       
-        Pessoa pessoa = new Pessoa("João", 30);
-        System.out.println("\nPessoa Comum:");
-        System.out.println(pessoa.toString());
-
-       
-        PessoaPessoa pessoaPessoa = new PessoaPessoa("Ana", 40);
-        System.out.println("\nPessoa Pessoa:");
-        System.out.println(pessoaPessoa.toString());
+        PessoaPessoa operacoesPessoaJuridica = new PessoaJuridica("Empresa XYZ", 987654321);
+        operacoesPessoaJuridica.criar();
+        operacoesPessoaJuridica.ler();
     }
 }
