46600a0f0a4de1b503a66a619bd8338643f19f88
Gabrielbscoa <107504001+Gabrielbscoa@users.noreply.github.com>
2023-12-04 14:37:10 -0300

    Update Pessoa.java

--- CrudHeranca/src/extend/Pessoa.java
+++ CrudHeranca/src/extend/Pessoa.java
@@ -1,13 +1,11 @@
-package extend;
-
 public class Pessoa {
-    
     private String nome;
-    private int idade;
 
-   public Pessoa(String nome, int idade) {
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome) {
         this.nome = nome;
-        this.idade = idade;
     }
 
     public String getNome() {
@@ -16,21 +14,5 @@ public class Pessoa {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                ", nome='" + nome + '\'' +
-                ", idade=" + idade +
-                '}';
     }
 }
