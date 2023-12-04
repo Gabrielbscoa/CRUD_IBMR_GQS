9f060010083997a31caa97b3ad6959dce17afdcf
Gabrielbscoa <107504001+Gabrielbscoa@users.noreply.github.com>
2023-12-04 14:38:43 -0300

    Update PessoaFisica.java

--- CrudHeranca/src/extend/PessoaFisica.java
+++ CrudHeranca/src/extend/PessoaFisica.java
@@ -1,28 +1,43 @@
-package extend;
+public class PessoaFisica extends Pessoa implements OperacoesCRUD {
+    private int cpf;
 
-public class PessoaFisica extends Pessoa {
-    
-    private String cpf;
+    public PessoaFisica() {
+    }
 
-     public PessoaFisica(String nome, int idade, String cpf) {
-        super(nome, idade);
+    public PessoaFisica(String nome, int cpf) {
+        super(nome);
         this.cpf = cpf;
     }
 
-    public String getCpf() {
+    public int getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
+    public void setCpf(int cpf) {
         this.cpf = cpf;
     }
 
     @Override
-    public String toString() {
-        return "PessoaFisica{" +
-                ", nome='" + getNome() + '\'' +
-                ", idade=" + getIdade() +
-                ", cpf='" + cpf + '\'' +
-                '}';
+    public void criar() {
+        // Implementation for creating Pessoa Física
+        System.out.println("Criando Pessoa Física: " + getNome());
+    }
+
+    @Override
+    public void ler() {
+        // Implementation for reading Pessoa Física
+        System.out.println("Lendo Pessoa Física: " + getNome());
+    }
+
+    @Override
+    public void atualizar() {
+        // Implementation for updating Pessoa Física
+        System.out.println("Atualizando Pessoa Física: " + getNome());
+    }
+
+    @Override
+    public void deletar() {
+        // Implementation for deleting Pessoa Física
+        System.out.println("Deletando Pessoa Física: " + getNome());
     }
 }
